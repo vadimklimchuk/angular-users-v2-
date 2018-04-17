1bc856828c52fbdf21759f80f03bccc88ff65a1d
Vadim Klimchuk <vadimklimchuk77@gmail.com>
2018-04-18 01:59:32 +0300

    add FooterComponent

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  footerTitle = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, accusantium!';
+}
